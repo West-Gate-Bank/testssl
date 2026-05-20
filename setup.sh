f6cffe4614ec2d7e07419ac53c1e20262521abaf
Cody Chapman <cchapman@wgb-rhel-pod01.westgatemain.com>
2026-05-20 16:54:16 -0500

    changes made to make it compliant with podman and quadlet

--- setup.sh
+++ setup.sh
@@ -9,3 +9,7 @@ echo "myorigin = $EMAIL_DOMAIN" > /etc/postfix/main.cf
 echo "relayhost = [$EMAIL_SERVER]"  >> /etc/postfix/main.cf
 echo "inet_interfaces = loopback-only" >> /etc/postfix/main.cf
 echo "mydestination = " >> /etc/postfix/main.cf
+echo "$RECIPIENT_TO" > /data/emailaddress.txt
+echo "To: $RECIPIENT_TO" > /data/email.txt
+echo "From: RECIPIENT_FROM" >> /data/email.txt
+echo "Subject: $SUBJECT" >> /data/email.txt
